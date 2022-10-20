047b9a6c8de0ee464a6063e175feb6cbbe4a1e16
shibili0 <90345492+shibili0@users.noreply.github.com>
2022-10-20 12:49:49 +0530

    small changes

--- script1.js
+++ script1.js
@@ -1,1 +1,14 @@
-let column = document.getElementsByClassName("new")
+const container = document.querySelector('.container')
+const seats = document.querySelectorAll('.row1 .seats:not(.booked)')
+const total = document.getElementById('total')
+let numberTOtal = +total.textContent
+const selectedSeats = document.getElementById("selectedSeats")
+container.addEventListener('click',(e)=> {
+    if(e.target.classList.contains('seats') && !e.target.classList.contains('booked'))
+
+{e.target.classList.toggle('selected')
+console.log(e.target.textContent)
+selectedSeats.textContent += e.target.textContent + ","
+total.textContent= ""
+}
+})
